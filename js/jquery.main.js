0e2cab5d2557eaf1980f05fb6e71fda668e4e980
yuriy777 <diesel424@rambler.ru>
2017-04-20 20:42:49 +0300

    font add and layout

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -8,3 +8,41 @@
     });
   }, false);
 })();
+
+$(function() {
+    var topButton = document.getElementsByClassName('go-top')[0];
+        window.addEventListener('scroll',function() {
+            var bdy = document.querySelector('body');
+            var doc = document.documentElement;
+
+              if(bdy.scrollTop > 800 || doc.scrollTop > 800) {
+                topButton.classList.add('show');
+              } else {
+                topButton.classList.remove('show');
+              }
+          },false);
+
+    function runScroll() {
+      scrollTo(bdy, 0, 300);
+    }
+
+    $(".navigation a").click(function() {
+      var elementClick = $(this).attr("href")
+      var destination = $(elementClick).offset().top;
+      var body = document.body;
+      jQuery("html:not(:animated),body:not(:animated)").animate({
+        scrollTop: destination
+      }, 800);
+      body.classList.remove('menu-open');
+      return false;
+    });
+    $(".go-top").click(function() {
+      var elementClick = $(this).attr("href")
+      var destination = $(elementClick).offset().top;
+      jQuery("html:not(:animated),body:not(:animated)").animate({
+        scrollTop: destination
+      }, 800);
+      return false;
+    });
+    
+  });
